d0f9eae32d7c6beabf696b059286f50a6384dc60
Ali Farooqi <ali.farooqi@kmon.net>
2025-12-13 17:43:35 +0800

    linted sitemaps

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -6,14 +6,14 @@ export const revalidate = 12*3600; // regenerate every hour
 
 export default async function sitemap() {
   const posts = await getMediumPosts()
-  let blogs = posts.map((post) => ({
+  const blogs = posts.map((post) => ({
     url: `${baseUrl}/blog/${post.slug}`,
     lastModified: post.date 
       ? new Date(post.date).toISOString().split('T')[0] 
       : new Date().toISOString().split('T')[0],
   }))
 
-  let routes = ['', '/blog'].map((route) => ({
+  const routes = ['', '/blog'].map((route) => ({
     url: `${baseUrl}${route}`,
     lastModified: new Date().toISOString().split('T')[0],
   }))
