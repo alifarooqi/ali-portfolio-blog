79c9a6fd43a557a6355108a11dc6099caf44188e
Ali Farooqi <ali.farooqi@kmon.net>
2025-12-13 22:10:00 +0800

    Prittier on icons

--- app/components/icons/svgr-motion-template.js
+++ app/components/icons/svgr-motion-template.js
@@ -1,12 +1,9 @@
 /**
- * 
+ *
  * Usage:
  *        npx @svgr/cli ./ --template svgr-motion-template.js --out-dir ./ --ext tsx --no-svgo
  * */
-module.exports = (
-  { imports, interfaces, componentName, props, jsx, exports },
-  { tpl }
-) => {
+module.exports = ({ imports, interfaces, componentName, props, jsx, exports }, { tpl }) => {
   let jsxString = jsx;
   // Generate final component
   return tpl`
